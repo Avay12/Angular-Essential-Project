e4a4d9ac2327bca339654fc93047f8d3e4f090e9
Avay12 <avaygc@gmail.com>
2024-08-05 15:29:08 +0545

    Angular Essentail Project

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,45 @@
+import {
+  Component,
+  computed,
+  EventEmitter,
+  input,
+  Input,
+  output,
+  Output,
+  signal,
+} from '@angular/core';
+import { type User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+  imports: [CardComponent],
+})
+export class UserComponent {
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
